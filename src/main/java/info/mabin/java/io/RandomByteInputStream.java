18b439e6675349370957b32b19b37b55c9d5e6ca
Mabin <mabin359@gmail.com>
2016-11-13 01:19:57 +0900

    RandomByteInputStream: Add Comments

--- src/main/java/info/mabin/java/io/RandomByteInputStream.java
+++ src/main/java/info/mabin/java/io/RandomByteInputStream.java
@@ -4,6 +4,10 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * Random Byte Input Stream<br/>
+ * Make Random Byte Stream
+ */
 @SuppressWarnings("unused")
 public class RandomByteInputStream extends InputStream{
     private static final int DEFAULT_SIZE = Integer.MAX_VALUE;
@@ -19,27 +23,58 @@ public class RandomByteInputStream extends InputStream{
     private ReadFunction readFunction;
     private SaveFunction saveFunction;
 
+    /**
+     * Constructs a RandomByteInputStream
+     */
     public RandomByteInputStream(){
         this(DEFAULT_SIZE, DEFAULT_KEEP_DATA, DEFAULT_USE_VISIBLE_BYTE);
     }
 
+    /**
+     * Constructs a RandomByteInputStream<br/>
+     * with Size
+     * @param size the size of stream data
+     */
     public RandomByteInputStream(int size){
         this(size, DEFAULT_KEEP_DATA, DEFAULT_USE_VISIBLE_BYTE);
     }
 
+    /**
+     * Constructs a RandomByteInputStream<br/>
+     * with Size and KeepData Option
+     * @param size the size of stream data
+     * @param keepData keep stream data. kept data can be get with {@link #toByteArray()}
+     */
     public RandomByteInputStream(int size, boolean keepData){
         this(size, keepData, DEFAULT_USE_VISIBLE_BYTE);
     }
 
+    /**
+     * Constructs a RandomByteInputStream<br/>
+     * with KeepData Option
+     * @param keepData keep stream data. kept data can be get with {@link #toByteArray()}
+     */
     public RandomByteInputStream(boolean keepData){
         this(DEFAULT_SIZE, keepData, DEFAULT_USE_VISIBLE_BYTE);
     }
 
+    /**
+     * Constructs a RandomByteInputStream<br/>
+     * with KeepData and UseVisibleByte Option
+     * @param keepData keep stream data. kept data can be get with {@link #toByteArray()}
+     * @param useVisibleByte make stream data with only visible characters.
+     */
     public RandomByteInputStream(boolean keepData, boolean useVisibleByte){
         this(DEFAULT_SIZE, keepData, useVisibleByte);
     }
 
-
+    /**
+     * Constructs a RandomByteInputStream<br/>
+     * with All Options
+     * @param size the size of stream data
+     * @param keepData keep stream data. kept data can be get with {@link #toByteArray()}
+     * @param useVisibleByte make stream data with only visible characters.
+     */
     public RandomByteInputStream(int size, boolean keepData, boolean useVisibleByte){
         this.size = size;
         this.keepData = keepData;
@@ -76,22 +111,34 @@ public class RandomByteInputStream extends InputStream{
         }
     }
 
+    /**
+     * Get kept Data.
+     * @return kept byte array
+     */
     public byte[] toByteArray(){
         return saveFunction.toByteArray();
     }
 
 
-
+    /**
+     * @param size the size of stream data
+     */
     public void setSize(int size) {
         this.size = size;
         init();
     }
 
+    /**
+     * @param keepData keep stream data. kept data can be get with {@link #toByteArray()}
+     */
     public void setKeepData(boolean keepData) {
         this.keepData = keepData;
         init();
     }
 
+    /**
+     * @param useVisibleByte make stream data with only visible characters.
+     */
     public void setUseVisibleByte(boolean useVisibleByte) {
         this.useVisibleByte = useVisibleByte;
         init();
